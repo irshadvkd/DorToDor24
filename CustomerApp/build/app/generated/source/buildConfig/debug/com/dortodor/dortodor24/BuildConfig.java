517a156f7cbd2f2480501dd9a520dc9d93833991
m22ca032 <m22ca032@gmail.com>
2024-08-24 02:13:51 +0530

    app files and code

--- CustomerApp/build/app/generated/source/buildConfig/debug/com/dortodor/dortodor24/BuildConfig.java
+++ CustomerApp/build/app/generated/source/buildConfig/debug/com/dortodor/dortodor24/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.dortodor.dortodor24;
+package com.dortodor.dorTodor24;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.dortodor.dortodor24";
+  public static final String APPLICATION_ID = "com.dortodor.dorTodor24";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.0";
