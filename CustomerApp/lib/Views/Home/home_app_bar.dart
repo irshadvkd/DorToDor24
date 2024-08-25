ebd05502dce85342c453876dee08f5db3992aa21
DANISH-T-P <danishtp11@gmail.com>
2024-08-25 22:55:16 +0530

    appbar edited

--- CustomerApp/lib/Views/Home/home_app_bar.dart
+++ CustomerApp/lib/Views/Home/home_app_bar.dart
@@ -8,6 +8,8 @@ import 'package:dorTodor24/Views/Product/cart_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../Auth/place_page.dart';
+
 class HomeAppBar extends StatefulWidget {
   const HomeAppBar({super.key});
 
@@ -43,6 +45,15 @@ class _HomeAppBarState extends State<HomeAppBar> {
                   .copyWith(color: colors.white),
             ),
             actions: [
+              IconButton(
+                icon: const Icon(
+                  Icons.location_city,
+                  color: colors.white,
+                ),
+                onPressed: () {
+                  Get.to(() => const PlaceSelectPage());
+                },
+              ),
               IconButton(
                 icon: const Icon(
                   Icons.notifications,
